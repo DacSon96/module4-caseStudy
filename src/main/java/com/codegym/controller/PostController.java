93abc60447cee0323357236f6408b2d4f830e628
toan.tran <toannd1997@email.com>
2021-11-08 09:12:43 +0700

    create post

--- src/main/java/com/codegym/controller/PostController.java
+++ src/main/java/com/codegym/controller/PostController.java
@@ -1,26 +1,46 @@
 package com.codegym.controller;
 
+import com.codegym.model.dto.PostForm;
 import com.codegym.model.entity.Post;
 import com.codegym.model.entity.User;
 import com.codegym.service.post.IPostService;
 import com.codegym.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Optional;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+
+    @Value("${file-upload}")
+    private String fileUpload;
+
     @Autowired
     private IPostService postService;
-    @Autowired
-    private IUserService userService;
+
 
     @PostMapping
-    public ResponseEntity<Post> createPost( @RequestBody Post post) {
+    public ResponseEntity<Post> createPost(PostForm postForm) {
+        MultipartFile multipartFile = postForm.getImg();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(postForm.getImg().getBytes(), new File(fileUpload + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Post post = new Post();
+        post.setUser(postForm.getUser());
+        post.setContent(postForm.getContent());
+        post.setImg(fileName);
         return new ResponseEntity<>(postService.save(post), HttpStatus.CREATED);
     }
 
